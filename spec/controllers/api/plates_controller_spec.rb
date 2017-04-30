4b4db0a5e464e53a956ab726bc420ce7eb92e361
Juan Gonzalez <jags84@gmail.com>
2017-04-30 12:02:56 -0400

    verification modal, rspec

--- spec/controllers/api/plates_controller_spec.rb
+++ spec/controllers/api/plates_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
+require 'devise'
 
 RSpec.describe Api::PlatesController, type: :controller do
+  include Devise::Test::ControllerHelpers
+  login_user
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, format: :json, params: {plate_number: 'AAA'}
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "returns 406 status without json format" do
+      get :show, params: {plate_number: 'AAA'}
+      is_expected.to respond_with 406
+    end
+
+  end
 end
