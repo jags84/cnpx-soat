ea358f37401de972ebce234f1b3ec8b8697f5cf4
Juan Gonzalez <jags84@gmail.com>
2017-04-29 21:19:27 -0400

    jobs specs

--- spec/jobs/user_mailer_job_spec.rb
+++ spec/jobs/user_mailer_job_spec.rb
@@ -1,5 +1,17 @@
-# require 'rails_helper'
-#
-# RSpec.describe UserMailerJob, type: :job do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe UserMailerJob, type: :job do
+  include ActiveJob::TestHelper
+  subject(:job) { described_class.perform_later(FactoryGirl.create(:policy,policy_status:'paid')) }
+
+  it 'queues the job' do
+    expect { job }
+      .to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+
+end
